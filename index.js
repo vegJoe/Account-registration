0068c8496e1ecbf53f3624aa902b8151f6a8ee95
Karsten Tvarnö <vvarrior@gmail.com>
2024-07-09 09:46:04 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,100 @@
+const inputForm = document.querySelector(".inputForm");
+const passwordListener = inputForm.querySelector("#password");
+const confirmPasswordListener = inputForm.querySelector("#confirmPassword");
+let btnClick = inputForm.querySelector(".btn");
+
+let passwordValid = false;
+let passwordLength = false;
+
+let d = document, [inputs, btn] = [
+    d.querySelectorAll('[type="text"], [type="email"], [type="password"]'),
+    d.querySelector(".btn")]
+    btn.disabled = false;
+
+for (i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener("input", () => {
+        let values = []
+        inputs.forEach( v => values.push(v.value))
+        btn.disabled = values.includes("")
+    })
+}
+
+passwordListener.addEventListener("input", (event) => {
+    if(validatePassword(passwordListener.value)) {
+        passwordLength = true;
+        if(!confirmPasswordListener.value === "")
+            document.querySelector(".btn").disabled = false;
+        else {
+            document.querySelector(".btn").disabled = true;
+        }
+
+        confirmPasswordListener.disabled = false;
+    }
+    else {
+        passwordLength = false;
+        confirmPasswordListener.value = "";
+        document.querySelector(".btn").disabled = true;
+        confirmPasswordListener.disabled = true;
+    }   
+})
+
+confirmPasswordListener.addEventListener("input", (event) => {
+    if(confirmPasswordListener.value === passwordListener.value) {
+        passwordValid = true;
+        document.querySelector(".btn").disabled = false;
+    }  
+    else {
+        passwordValid = false;
+        document.querySelector(".btn").disabled = true;
+    }
+})
+
+btnClick.addEventListener("mouseover", (event) => {
+    const name = inputForm.querySelector("#name");
+    const userName = inputForm.querySelector("#username");
+    const email = inputForm.querySelector("#email");
+
+    if(name.value === "" || username.value === "" || email.value === "") {
+        window.alert("Make sure you fill in all the fields");
+    }
+    else if(!passwordLength) {
+        window.alert("Password length need to be 8 characters minimum!");
+    }
+    else if(confirmPasswordListener.value !== passwordListener.value) {
+        window.alert("Password does not match");
+    }
+})
+
+inputForm.addEventListener("submit", (event) => {event.preventDefault();
+
+    const name = inputForm.querySelector("#name");
+    const userName = inputForm.querySelector("#username");
+    const email = inputForm.querySelector("#email");
+    const password = inputForm.querySelector("#password");
+    const confirmPassword = inputForm.querySelector("#confirmPassword");
+
+    const nameValue = name.value;
+    const userNameValue = userName.value;
+    const emailValue = email.value;
+    let passwordValue = "";
+    if(passwordValid)
+        passwordValue = password.value;
+    const confirmPasswordValue = confirmPassword.value;
+
+    const user = {
+        name: nameValue,
+        username: userNameValue,
+        email: emailValue,
+        password: passwordValue
+    }
+
+    console.log(user);
+});
+
+function validatePassword(password) {
+    if(password.length > 8) {
+        return true;
+    }
+    else
+        return false;
+};
